3966d9cd24cfd745075ad3a6696e1c886468f025
jpres <john.presley.27@gmail.com>
2023-12-12 01:30:26 -0500

    printf debugging added to create_instance()

--- render.cpp
+++ render.cpp
@@ -1,5 +1,7 @@
 #include "render.h"
 
+#include <cstdio>
+
 struct TeWindow te_window;
 
 void render_loop(TeWindow *te_window) {
@@ -10,7 +12,9 @@ void render_loop(TeWindow *te_window) {
 
 void render() {
     init_window(HEIGHT, WIDTH, "Ave!", &te_window);
-    create_instance();
+    if(create_instance() < 0) {
+        printf("Failed to create instance \n");
+    }
     render_loop(&te_window);
     destroy_window(&te_window);
 }
