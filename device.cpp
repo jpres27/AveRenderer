74740aef5fb8ce50bfb8ddff76ac1c94b60cd3d3
jpres <john.presley.27@gmail.com>
2023-12-12 13:51:17 -0500

    added available and required extensions query

--- device.cpp
+++ device.cpp
@@ -1,6 +1,24 @@
 #include "device.h"
 
+#include <vector>
+#include <cstdio>
+#include <unordered_set>
+
 VkInstance instance;
+
+std::vector<const char *> get_required_extensions() {
+  uint32_t glfwExtensionCount = 0;
+  const char **glfwExtensions;
+  glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+  std::vector<const char *> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+//  if (enableValidationLayers) {
+//    extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+//  }
+
+  return extensions;
+}
 
 int create_instance() {
     VkApplicationInfo app_info{};
@@ -21,6 +39,29 @@ int create_instance() {
     glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
     create_info.enabledExtensionCount = glfwExtensionCount;
     create_info.ppEnabledExtensionNames = glfwExtensions;
+
+    // Retrieve list of supported extensions
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+    std::unordered_set<std::string> available;
+    printf("Available extensions: \n");
+    for (const auto& extension : extensions) {
+        printf("%s\n", extension.extensionName);
+        available.insert(extension.extensionName);
+    }
+    
+    // See if required GLFW extensions are present
+    printf("\nRequired extensions: \n");
+    auto requiredExtensions = get_required_extensions();
+    for (const auto &required : requiredExtensions) {
+        printf("%s\n", required);
+        if (available.find(required) == available.end()) {
+            printf("Missing required GLFW extension");
+            return -1;
+        }
+    }   
 
     create_info.enabledLayerCount = 0;
     if(vkCreateInstance(&create_info, nullptr, &instance) != VK_SUCCESS) {
